0ea43188915864226131b0eabdef81f5aa153221
Sergio <fhict@macbook-pro-2.local>
2016-03-10 11:39:22 +0100

    Initial Commit

---
+++ Hellglow World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hellglow World
+//
+//  Created by Sergio  on 10/03/16.
+//  Copyright © 2016 Fhict. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
